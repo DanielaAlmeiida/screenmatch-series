348b097156c309d4df421b1d8ed062e6aa569a8a
Daniela Almeida <danielaspbrasil@gmail.com>
2024-03-11 10:16:21 -0300

    Criação da SerieService e melhoria na SerieController

--- src/main/java/br/com/alura/screenmatchserie/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatchserie/controller/SerieController.java
@@ -2,30 +2,31 @@ package br.com.alura.screenmatchserie.controller;
 
 import br.com.alura.screenmatchserie.dto.SerieDTO;
 import br.com.alura.screenmatchserie.repository.SerieRepository;
+import br.com.alura.screenmatchserie.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
-    private SerieRepository serieRepository;
+    private SerieService serieService;
 
-    @GetMapping("/series")
+    @GetMapping
     public List<SerieDTO> obterSeries() {
-        return serieRepository.findAll()
-                .stream()
-                .map(s -> new SerieDTO( s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
+        return  serieService.obterTodasAsSeries();
     }
 
-    @GetMapping("/inicio")
-    public String retornarInicio() {
-        return "Bem-vindo ao Screenmatch!";
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series() {
+        return serieService.obterTop5Series();
     }
 
 }
